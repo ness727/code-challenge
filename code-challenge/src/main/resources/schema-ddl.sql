c1c9bdc9f99ead6e27bc4a025c2755ecf2709870
ness727 <gnsdl727@naver.com>
2024-10-04 11:12:35 +0900

    [ness727] DB 스키마 변경

--- code-challenge/src/main/resources/schema-ddl.sql
+++ code-challenge/src/main/resources/schema-ddl.sql
@@ -4,8 +4,8 @@ create table users(
     provider_id varchar(50),
     nickname varchar(50),
     score integer,
-    created_at timestamp default current_timestamp on update current_timestamp,
-    updated_at timestamp default current_timestamp on update current_timestamp
+    created_at datetime default current_timestamp on update current_timestamp,
+    updated_at datetime default current_timestamp on update current_timestamp
 );
 
 create table user_problems(
@@ -13,15 +13,16 @@ create table user_problems(
     user_id bigint,
     problem_id bigint,
     answer text,
-    user_problem_status_id integer,
-    created_at timestamp default current_timestamp on update current_timestamp,
-    updated_at timestamp default current_timestamp on update current_timestamp
+    # user_problem_status_id integer,
+    status tinyint,
+    created_at datetime default current_timestamp on update current_timestamp,
+    updated_at datetime default current_timestamp on update current_timestamp
 );
 
-create table user_problem_status(
-    id integer primary key auto_increment,
-    name varchar(50)
-);
+# create table user_problem_status(
+#     id integer primary key auto_increment,
+#     name varchar(50)
+# );
 
 create table problems(
     id bigint primary key auto_increment,
@@ -29,8 +30,16 @@ create table problems(
     title varchar(255),
     content text,
     problem_picture_id bigint,
-    created_at timestamp default current_timestamp on update current_timestamp,
-    updated_at timestamp default current_timestamp on update current_timestamp
+    testcase_id bigint,
+    created_at datetime default current_timestamp on update current_timestamp,
+    updated_at datetime default current_timestamp on update current_timestamp
+);
+
+create table testcases(
+    id bigint primary key auto_increment,
+    content varchar(255),
+    data_type tinyint,
+    type tinyint
 );
 
 create table problem_pictures(
@@ -39,16 +48,16 @@ create table problem_pictures(
     url varchar(255)
 );
 
-create table levels(
-    id integer primary key auto_increment,
-    name varchar(20)
-);
+# create table levels(
+#     id integer primary key auto_increment,
+#     name varchar(20)
+# );
 
 create table user_badges(
     id bigint primary key auto_increment,
     user_id bigint,
     badge_id integer,
-    created_at timestamp default current_timestamp on update current_timestamp
+    created_at datetime default current_timestamp on update current_timestamp
 );
 
 create table badges(
@@ -59,8 +68,9 @@ create table badges(
 
 alter table user_problems add foreign key(user_id) references users(id);
 alter table user_problems add foreign key(problem_id) references problems(id);
-alter table user_problems add foreign key(user_problem_status_id) references user_problem_status(id);
-alter table problems add foreign key(level_id) references levels(id);
+# alter table user_problems add foreign key(user_problem_status_id) references user_problem_status(id);
+# alter table problems add foreign key(level_id) references levels(id);
 alter table problems add foreign key(problem_picture_id) references problem_pictures(id);
+alter table problems add foreign key(testcase_id) references testcases(id);
 alter table user_badges add foreign key(user_id) references users(id);
 alter table user_badges add foreign key(badge_id) references badges(id);
