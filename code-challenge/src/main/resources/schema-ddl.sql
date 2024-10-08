3b8a4bd9f9c136bd971a6420e57bffbae13a4629
ness727 <gnsdl727@naver.com>
2024-10-08 20:54:04 +0900

    ddl 파일 정리

--- code-challenge/src/main/resources/schema-ddl.sql
+++ code-challenge/src/main/resources/schema-ddl.sql
@@ -13,16 +13,10 @@ create table user_problems(
     user_id bigint,
     problem_id bigint,
     answer text,
-    # user_problem_status_id integer,
     status tinyint,
     created_at datetime default current_timestamp on update current_timestamp,
     updated_at datetime default current_timestamp on update current_timestamp
 );
-
-# create table user_problem_status(
-#     id integer primary key auto_increment,
-#     name varchar(50)
-# );
 
 create table problems(
     id bigint primary key auto_increment,
@@ -50,11 +44,6 @@ create table problem_pictures(
     url varchar(255)
 );
 
-# create table levels(
-#     id integer primary key auto_increment,
-#     name varchar(20)
-# );
-
 create table user_badges(
     id bigint primary key auto_increment,
     user_id bigint,
@@ -70,8 +59,6 @@ create table badges(
 
 alter table user_problems add foreign key(user_id) references users(id);
 alter table user_problems add foreign key(problem_id) references problems(id);
-# alter table user_problems add foreign key(user_problem_status_id) references user_problem_status(id);
-# alter table problems add foreign key(level_id) references levels(id);
 alter table testcases add foreign key(problem_id) references problems(id);
 alter table problem_pictures add foreign key(problem_id) references problems(id);
 alter table user_badges add foreign key(user_id) references users(id);
