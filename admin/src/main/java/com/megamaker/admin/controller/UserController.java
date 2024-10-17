a506671f0eb0948414683739b82633233ab0eb31
ness727 <pinpanel237@gmail.com>
2024-10-17 15:43:54 +0900

    이전 파일 포함

--- admin/src/main/java/com/megamaker/admin/controller/UserController.java
+++
@@ -1,9 +0,0 @@
-package com.megamaker.admin.controller;
-
-import com.megamaker.admin.service.UserService;
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class UserController {
-    private final UserService userService;
-}
