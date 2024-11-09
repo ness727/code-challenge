16d4b460e3faf7fb20c33cf46d6dc03c7837cb15
ness727 <pinpanel237@gmail.com>
2024-11-09 20:37:25 +0900

    유저 정보 응답 추가

--- code-challenge/src/main/java/com/megamaker/codechallenge/controller/UserController.java
+++ code-challenge/src/main/java/com/megamaker/codechallenge/controller/UserController.java
@@ -1,31 +1,41 @@
 package com.megamaker.codechallenge.controller;
 
 import com.megamaker.codechallenge.dto.user.RequestUserEdit;
+import com.megamaker.codechallenge.dto.user.ResponseUser;
 import com.megamaker.codechallenge.securityconfig.oauth2.CustomOAuth2User;
 import com.megamaker.codechallenge.service.UserService;
 import com.megamaker.codechallenge.service.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/user")
 @RestController
 public class UserController {
     private final UserService userService;
 
-//    @GetMapping
-//    public ResponseUser get(Auth auth) {
-//
-//    }
+    @GetMapping
+    public ResponseUser get(Authentication auth) {
+        CustomOAuth2User oAuth2User = getCustomOAuth2User(auth);
+
+        return userService.get(oAuth2User.getProviderId());
+    }
 
     @PutMapping
     public ResponseEntity edit(Authentication auth, @RequestBody RequestUserEdit requestUserEdit) {
-        if (auth == null) throw new UserNotFoundException();
-        CustomOAuth2User oAuth2User = (CustomOAuth2User) auth.getPrincipal();
+        CustomOAuth2User oAuth2User = getCustomOAuth2User(auth);
 
         userService.edit(oAuth2User.getProviderId(), requestUserEdit);
         return ResponseEntity.noContent().build();
+    }
+
+    private static CustomOAuth2User getCustomOAuth2User(Authentication auth) {
+        if (auth == null) throw new UserNotFoundException();
+        CustomOAuth2User oAuth2User = (CustomOAuth2User) auth.getPrincipal();
+        return oAuth2User;
     }
 }
--- code-challenge/src/main/java/com/megamaker/codechallenge/service/UserService.java
+++ code-challenge/src/main/java/com/megamaker/codechallenge/service/UserService.java
@@ -1,23 +1,34 @@
 package com.megamaker.codechallenge.service;
 
 import com.megamaker.codechallenge.dto.user.RequestUserEdit;
+import com.megamaker.codechallenge.dto.user.ResponseUser;
 import com.megamaker.codechallenge.entity.User;
+import com.megamaker.codechallenge.mapper.UserMapper;
 import com.megamaker.codechallenge.repository.UserRepository;
 import com.megamaker.codechallenge.service.exception.UserNotFoundException;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
+@Transactional
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
 
-    @Transactional
+    public ResponseUser get(String providerId) {
+        User foundUser = userRepository.findByProviderId(providerId)
+                .orElseThrow(UserNotFoundException::new);
+
+        log.info(foundUser.getProviderNickname());
+        return UserMapper.INSTANCE.toResponseUser(foundUser);
+    }
+
     public void edit(String providerId, RequestUserEdit requestUserEdit) {
         User foundUser = userRepository.findByProviderId(providerId)
                 .orElseThrow(UserNotFoundException::new);
-        foundUser.setNickname(requestUserEdit.getNickname());
+        foundUser.setProviderNickname(requestUserEdit.getNickname());
     }
 }
