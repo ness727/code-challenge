edd4e2c2ee7f3c7075997c9d08fce0fa54d80f7f
ness727 <pinpanel237@gmail.com>
2024-11-11 11:31:36 +0900

    뱃지 상위 유저 3명 달성 로직 추가

--- code-challenge/src/main/java/com/megamaker/codechallenge/repository/UserRepository.java
+++ code-challenge/src/main/java/com/megamaker/codechallenge/repository/UserRepository.java
@@ -1,10 +1,15 @@
 package com.megamaker.codechallenge.repository;
 
 import com.megamaker.codechallenge.entity.User;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByProviderId(String providerId);
+
+    // 점수 상위 유저 3명 조회
+    List<User> findTop3ByOrderByScoreDesc();
 }
